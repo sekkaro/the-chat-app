e777557c5e6b44a37511e6af9940f5c561864580
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-09-15 12:55:22 +0900

    broadcast when user join & leaves

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,14 @@ const main = () => {
     console.log("new web socket connection");
 
     socket.emit("message", "Welcome!");
+    socket.broadcast.emit("message", "A new user has joined!");
 
     socket.on("sendMessage", (message) => {
       io.emit("message", message);
+    });
+
+    socket.on("disconnect", () => {
+      io.emit("message", "A user has left");
     });
   });
 
